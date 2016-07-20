60488826e97ca0d979f767d8cfb2890283cbeb11
evan <liusuzheng2012@gmail.com>
2016-07-20 16:16:52 +0800

    build android

--- android/run-build-bundle.sh
+++ android/run-build-bundle.sh
@@ -1,3 +1,3 @@
 rm -rf $TMPDIR/react-*
 rm -f android/app/src/main/assets/index.android.bundle
-react-native bundle --minif --platform android --dev false --entry-file index.android.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
+react-native bundle --minif --platform android --dev false --entry-file index.android.js --bundle-output android/mylibrary/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
